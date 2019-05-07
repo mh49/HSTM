f48bb0216ef7cf63f6e2d8ad9885e127ad92f956
AbdelHamid <meb.hamid.94@gmail.com>
2019-05-07 14:12:43 +0100

    cleaned config.py in /app

--- app/config.py
+++ app/config.py
@@ -4,12 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'djvdskjfkfjlknfbuhdf'
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'app.db')
-        
-    # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-
-    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:azerty1@localhost:5432/HSTS' 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres+psycopg2://postgres:azerty1@localhost:5432/HSTS' 
 
     # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:root@toor@localhost:5432/HomeDB' 
     
